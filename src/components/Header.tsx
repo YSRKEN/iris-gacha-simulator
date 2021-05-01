e250dc6c8e6c25c3b740caaa66a5d12d41271bda
YSRKEN <ysr.ken@gmail.com>
2021-05-01 14:27:44 +0900

    [add] ヘッダー部分を分離させた

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Header: React.FC = () => <>
+  <h1 className="text-nowrap d-none d-md-block">『あいミス』ガチャシミュレーター</h1>
+  <h2 className="text-nowrap d-none d-sm-block d-md-none">『あいミス』ガチャシミュレーター</h2>
+  <h3 className="d-block d-sm-none">
+    <span className="text-nowrap">『あいミス』ガチャ</span>
+    <span className="text-nowrap">シミュレーター</span>
+  </h3>
+</>;
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,18 @@
 import * as React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import Header from '../components/Header';
 
-const Header: React.FC = () => <>
-  <h1 className="text-nowrap d-none d-md-block">『あいミス』ガチャシミュレーター</h1>
-  <h2 className="text-nowrap d-none d-sm-block d-md-none">『あいミス』ガチャシミュレーター</h2>
-  <h3 className="d-block d-sm-none">
-    <span className="text-nowrap">『あいミス』ガチャ</span>
-    <span className="text-nowrap">シミュレーター</span>
-  </h3>
-</>;
-
-const IndexPage = () => {
-  return (
-    <main>
-      <title>『あいミス』ガチャシミュレーター</title>
-      <Container>
-        <Row className="my-3">
-          <Col className="text-center">
-            <Header />
-          </Col>
-        </Row>
-      </Container>
-    </main>
-  )
-}
+const IndexPage: React.FC = () =>
+  <main>
+    <title>『あいミス』ガチャシミュレーター</title>
+    <Container>
+      <Row className="my-3">
+        <Col className="text-center">
+          <Header />
+        </Col>
+      </Row>
+    </Container>
+  </main>;
 
 export default IndexPage;
